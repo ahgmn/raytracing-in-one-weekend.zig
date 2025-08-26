d3baec9d059d0b38532a62cbfd999ae3e428f6a8
ahgmn <ahgmn@proton.me>
2025-08-26 20:13:05 +0300

    Vector class created

--- src/utils.zig
+++ src/utils.zig
@@ -1,8 +1,19 @@
 const m = @import("std").math;
+const Vector = @import("vector.zig");
+const Vec3f = Vector.Vec3f;
+const Col3f = Vector.Col3f;
+
 pub fn to_f32(comptime T: type, from: T) f32 {
     return @as(f32, @floatFromInt(from));
 }
 
 pub fn clamp_to_u8(comptime T: type, from: T) u8 {
     return @as(u8, @intFromFloat(m.clamp(from, 0.0, 1.0) * 255.999));
+}
+
+pub fn write_col_to(col: Col3f, writer: anytype) !void {
+    const r: u8 = clamp_to_u8(f32, col.r());
+    const g: u8 = clamp_to_u8(f32, col.g());
+    const b: u8 = clamp_to_u8(f32, col.b());
+    try writer.print("{} {} {}\n", .{ r, g, b });
 }
--- src/vector.zig
+++ src/vector.zig
@@ -1,17 +1,95 @@
-pub fn Vec(comptime dim: usize, comptime T: type) type {
-    return struct {
-        data: [dim]T,
+const std = @import("std");
 
-        pub fn init(values: [dim]T) @This() {
-            return @This(){ .data = values };
+pub const Vec3f = Vector3("Vec3f", f32);
+pub const Col3f = Vector3("Col3f", f32);
+
+pub fn Vector3(comptime TypeName: []const u8, comptime T: type) type {
+    return struct {
+        inner: @Vector(3, T),
+        pub const name = TypeName;
+
+        pub inline fn new(_x: T, _y: T, _z: T) @This() {
+            return .{ .inner = .{ _x, _y, _z } };
         }
 
-        pub fn add(first: @This(), second: @This()) @This() {
-            var result = @This(){ .data = undefined };
-            for (0..dim) |i| {
-                result.data[i] = first.data[i] + second.data[i];
-            }
-            return result;
+        // Getters
+        pub inline fn x(self: @This()) T {
+            return self.inner[0];
+        }
+        pub inline fn y(self: @This()) T {
+            return self.inner[1];
+        }
+        pub inline fn z(self: @This()) T {
+            return self.inner[2];
+        }
+        pub inline fn r(self: @This()) T {
+            return self.inner[0];
+        }
+        pub inline fn g(self: @This()) T {
+            return self.inner[1];
+        }
+        pub inline fn b(self: @This()) T {
+            return self.inner[2];
+        }
+
+        // Basic
+        pub fn add(u: @This(), v: @This()) @This() {
+            return .{ .inner = u.inner + v.inner };
+        }
+        pub fn adds(u: @This(), t: T) @This() {
+            return .{ .inner = u.inner + @as(@This(), @splat(t)) };
+        }
+        pub fn sub(u: @This(), v: @This()) @This() {
+            return .{ .inner = u.inner - v.inner };
+        }
+        pub fn subs(u: @This(), t: T) @This() {
+            return .{ .inner = u.inner - @as(@This(), @splat(t)) };
+        }
+        pub fn mul(u: @This(), v: @This()) @This() {
+            return .{ .inner = u.inner * v.inner };
+        }
+        pub fn muls(u: @This(), t: T) @This() {
+            return .{ .inner = u.inner * @as(@This(), @splat(t)) };
+        }
+        pub fn div(u: @This(), v: @This()) @This() {
+            return .{ .inner = u.inner / v.inner };
+        }
+        pub fn divs(u: @This(), t: T) @This() {
+            return .{ .inner = u.inner / @as(@This(), @splat(t)) };
+        }
+
+        // Vector
+        pub fn len(self: @This()) T {
+            return std.math.sqrt(self.len_sq());
+        }
+        pub fn len_sq(self: @This()) T {
+            return self.x * self.x + self.y * self.y + self.z * self.z;
+        }
+        pub fn dot(u: @This(), v: @This()) T {
+            return u.x * v.x + u.y * v.y + u.z * v.z;
+        }
+        pub fn cross(u: @This(), v: @This()) @This() {
+            return .{ .inner = .{
+                u.y * v.z - u.z * v.y,
+                u.z * v.x - u.x * v.z,
+                u.x * v.y - u.y * v.y,
+            } };
+        }
+        pub fn unit(self: @This()) @This() {
+            return self / self.len();
+        }
+
+        pub fn format(
+            self: @This(),
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = fmt; // unused here
+            _ = options; // could be used to change precision, etc.
+            try writer.print("({d:.3}, {d:.3}, {d:.3})", .{
+                self.x(), self.y(), self.z(),
+            });
         }
     };
 }
